36109ccb0eba1dd501691c495e4aaf017f6f7ce0
Nicolas Touzain <n.touzain@student.isartdigital.com>
2017-01-21 10:22:09 +0100

    UI next step

--- Assets/Scripts/Commons/BaseManager.cs
+++ Assets/Scripts/Commons/BaseManager.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public abstract class BaseManager<T>: Singleton<T> where T: Component {
     #region Initialisation & Destroy
     protected override void Start() {
         if (GameManager.instance) {
-            GameManager.instance.onMainScreen += MainScreen;
+            GameManager.instance.onMainScreen   += MainScreen;
+            GameManager.instance.onPlayGame     += PlayGame;
         }
         else DebugError("GameManager does not exist");
 
@@ -14,6 +16,7 @@ public abstract class BaseManager<T>: Singleton<T> where T: Component {
     protected override void Destroy() {
         if (GameManager.instance) {
             GameManager.instance.onMainScreen   -= MainScreen;
+            GameManager.instance.onPlayGame     -= PlayGame;
         }
 
         base.Destroy();
@@ -22,6 +25,10 @@ public abstract class BaseManager<T>: Singleton<T> where T: Component {
 
     #region Game Events
     protected virtual void MainScreen() {
+
+    }
+
+    protected virtual void PlayGame(Dictionary<int, int> p_PlayerInstrumentDictionnary) {
 
     }
     #endregion
--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class MenuManager: BaseManager<MenuManager> {
     #region Variables
@@ -8,14 +9,24 @@ public class MenuManager: BaseManager<MenuManager> {
     public GameObject Lobby;
     public GameObject Scores;
     public GameObject Credits;
+    public GameObject BTN_Launch;
     public List<GameObject> LobbyPlayerList;
+    public Action<Dictionary<int, int>> onLaunchGame;
 
     private GameObject m_CurrentScreen;
+    private Dictionary<int, int> m_PlayerInstrumentDictionnary;
+
+    public int playerNB {
+        get;
+        private set;
+    }
     #endregion
 
     #region Initialisation & Destroy
     override protected IEnumerator CoroutineStart() {
-        isReady = true;
+        playerNB                        = 0;
+        m_PlayerInstrumentDictionnary   = new Dictionary<int, int>();
+        isReady                         = true;
 
         yield return true;
     }
@@ -24,6 +35,10 @@ public class MenuManager: BaseManager<MenuManager> {
     #region Interface Managment
     protected override void MainScreen() {
         OpenScreen(TitleCard);
+    }
+
+    protected override void PlayGame(Dictionary<int, int> p_PlayerInstrumentDictionnary) {
+        DebugLogWarning(" nombre de joueur : " + p_PlayerInstrumentDictionnary.Keys.Count);
     }
 
     public void OnClicCredits() {
@@ -39,7 +54,39 @@ public class MenuManager: BaseManager<MenuManager> {
     }
 
     public void OnClicJoin(int p_PlayerID) {
+        SwitchLobbyPlayer(p_PlayerID, true);
+    }
 
+    public void OnClicLeave(int p_PlayerID) {
+        SwitchLobbyPlayer(p_PlayerID, false);
+    }
+
+    private void SwitchLobbyPlayer(int p_PlayerID, bool p_IsInstrumentActive) {
+        LobbyPlayerList[p_PlayerID - 1].transform.FindChild("Instruments").gameObject.SetActive(p_IsInstrumentActive);
+        LobbyPlayerList[p_PlayerID - 1].transform.FindChild("BTN_Join").gameObject.SetActive(!p_IsInstrumentActive);
+    }
+
+    public void OnClicPlayerInstrument(int p_PlayerIDInstrumentID) {
+        int l_PlayerID      = int.Parse(p_PlayerIDInstrumentID.ToString()[0].ToString());
+        int l_InstrumentID  = int.Parse(p_PlayerIDInstrumentID.ToString()[1].ToString());
+
+        if (m_PlayerInstrumentDictionnary.ContainsKey(l_PlayerID)) {//deselection
+            playerNB--;
+            //deverrouiller l'instrument deselectionne pour tout le monde (si joueur non verrouille)
+            //deverouiller les instruments non selectionne pour lui
+        }
+        else {//selection
+            playerNB++;
+            m_PlayerInstrumentDictionnary.Add(l_PlayerID, l_InstrumentID);
+            //verouille l'instrument pour les autres joueurs (si joueur non verrouille)
+            //verouille les autres instruments
+        }
+
+        BTN_Launch.SetActive(playerNB > 0);
+    }
+
+    public void OnClicLaunch() {
+        if (onLaunchGame != null) onLaunchGame(m_PlayerInstrumentDictionnary);
     }
 
     private void OpenScreen(GameObject p_ScreenToOpen) {
