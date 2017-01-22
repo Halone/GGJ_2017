b696e35ac9466927eca1094d618ca1686472409c
Nicolas Touzain <n.touzain@student.isartdigital.com>
2017-01-22 05:15:29 +0100

    sans FMOD

---
+++ Assets/Scripts/Utils/PointMesh.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointMesh : MonoBehaviour {
+
+	public bool IsMain;
+	public Vector2 Position;
+	public float Order;
+}
